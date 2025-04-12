7c5db629d3dd92d743fe5e1e91ed2f29648fc67c
Akashchandranvs <aakashchandranvs@gmail.com>
2025-04-12 10:27:08 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
+ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "AVIATRIX_AKASH_LAB"
     workspaces {
       name = "ace-iac-day-zero"
     }
